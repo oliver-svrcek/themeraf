806016913d2ecaefc4e4606aa2ff31cc42ad5d2b
Oliver Svrček <o.svrcek@icloud.com>
2023-04-04 21:15:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Themeraf (Zsh theme)
+
+This is my own Zsh theme.
+
+<img width="718" alt="Themeraf" src="https://user-images.githubusercontent.com/75705745/218309084-1bcd182e-44ba-4cb7-b272-88c26868eec9.png">
+
+It shows the username, hostname, last two directories in the working directory path, git branch name (color indicates git status), timestamp and last exit status.
+
+## Installation
+
+### Manual
+
+Just `source` the `Themeraf.zsh` file in the `.zshrc` file. 
+
+## Author
+Created by  Oliver Svrček
---
+++ Themeraf.zsh
@@ -0,0 +1,34 @@
+zstyle ':vcs_info:git:*' formats '%b'
+
+function repo_status_part() {
+    if [[ -n ${vcs_info_msg_0_} ]]
+        then
+        local REPO_STATUS=$(command git status --porcelain 2> /dev/null | tail -n1)
+                if [[ -n $REPO_STATUS ]]
+                then
+                        echo "${SEPARATOR}%F{214}${vcs_info_msg_0_}%f"
+        else
+                        echo "${SEPARATOR}%F{255}${vcs_info_msg_0_}%f"
+        fi
+    fi
+}
+
+function easter_egg_part() {
+    if [ $(seq 1000 | sort -R | head -1) -eq 420 ]; then
+        echo "(42 72 75 68) "
+    fi
+}
+
+local SEPARATOR="%F{235} · %f"
+local START_PART="%F{255}┍ %f"
+local USERNAME_PART="%B%F{226}%n%f%b"
+local HOSTNAME_PART="${SEPARATOR}%F{255}%m%f"
+local WORKING_DIR_PART="${SEPARATOR}%F{255}%2d%f"
+local REPO_STATUS_PART='$(repo_status_part)'
+local TIME_PART="${SEPARATOR}%F{255}%*%f"
+local EXIT_STATUS_PART="${SEPARATOR}%(?.%F{255}%?%f.%F{214}%?%f)"
+local END_PART="%F{255}┕ %f"
+local EASTER_EGG_PART='$(easter_egg_part)'
+
+PROMPT="${START_PART}${USERNAME_PART}${HOSTNAME_PART}${WORKING_DIR_PART}${REPO_STATUS_PART}${TIME_PART}${EXIT_STATUS_PART}
+${END_PART}${EASTER_EGG_PART}"
