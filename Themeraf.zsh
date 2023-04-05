6cb606f3ca19686510c6d6c85b775b2205720a50
Oliver Svrček <o.svrcek@icloud.com>
2023-04-05 20:25:31 +0200

    feat: add virtual environment part to the prompt, change separator

--- Themeraf.zsh
+++ Themeraf.zsh
@@ -1,3 +1,4 @@
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 setopt PROMPT_SUBST
 autoload -Uz vcs_info
 
@@ -7,15 +8,23 @@ precmd() {
 
 zstyle ':vcs_info:git:*' formats '%b'
 
+# venv
+function venv_part() {
+	if [[ -v VIRTUAL_ENV ]]; then
+		echo "%F{159}`basename $VIRTUAL_ENV`%f${SEPARATOR}"
+	fi
+}
+
+# git
 function repo_status_part() {
     if [[ -n ${vcs_info_msg_0_} ]]
         then
         local REPO_STATUS=$(command git status --porcelain 2> /dev/null | tail -n1)
-                if [[ -n $REPO_STATUS ]]
-                then
-                        echo "${SEPARATOR}%F{214}${vcs_info_msg_0_}%f"
-        else
-                        echo "${SEPARATOR}%F{255}${vcs_info_msg_0_}%f"
+        if [[ -n $REPO_STATUS ]]
+        then
+            echo "${SEPARATOR}%F{214}${vcs_info_msg_0_}%f"
+		else
+            echo "${SEPARATOR}%F{255}${vcs_info_msg_0_}%f"
         fi
     fi
 }
@@ -26,7 +35,8 @@ function easter_egg_part() {
     fi
 }
 
-local SEPARATOR="%F{235} · %f"
+local SEPARATOR="%F{235}│%f"
+local VENV_PART='$(venv_part)'
 local START_PART="%F{255}┍ %f"
 local USERNAME_PART="%B%F{226}%n%f%b"
 local HOSTNAME_PART="${SEPARATOR}%F{255}%m%f"
@@ -37,5 +47,5 @@ local EXIT_STATUS_PART="${SEPARATOR}%(?.%F{255}%?%f.%F{214}%?%f)"
 local END_PART="%F{255}┕ %f"
 local EASTER_EGG_PART='$(easter_egg_part)'
 
-PROMPT="${START_PART}${USERNAME_PART}${HOSTNAME_PART}${WORKING_DIR_PART}${REPO_STATUS_PART}${TIME_PART}${EXIT_STATUS_PART}
+PROMPT="${START_PART}${VENV_PART}${USERNAME_PART}${HOSTNAME_PART}${WORKING_DIR_PART}${REPO_STATUS_PART}${TIME_PART}${EXIT_STATUS_PART}
 ${END_PART}${EASTER_EGG_PART}"
